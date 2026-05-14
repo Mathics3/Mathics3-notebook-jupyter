af27fd31391eae7c4a90682bfa41fa8c27154948
rocky <rb@dustyfeet.com>
2026-05-14 13:13:10 -0400

    Bang on formatting.
    Mostly formatting of TeXForm and MathMLForm.

--- mathics3_jupyter_notebook/formatter.py
+++ mathics3_jupyter_notebook/formatter.py
@@ -2,41 +2,30 @@
 Jupyter Mathics3 Formatter module
 """
 
-from typing import Dict, Final
+from typing import Callable
 
 from mathics.core.atoms import String
+from mathics.core.expression import BoxError, Expression
+from mathics.core.symbols import Symbol
 from mathics.core.systemsymbols import (
     SymbolAborted,
     SymbolFailed,
+    SymbolFullForm,
+    SymbolInputForm,
     SymbolInterpretationBox,
+    SymbolMathMLForm,
     SymbolOutputForm,
     SymbolStandardForm,
+    SymbolString,
     SymbolTeXForm,
 )
 from mathics.format.box import format_element
 
-# import base64
-# from mathics.formatters import MathicsLaTeXFormatter
-
-
-# Maps a Form to a kind of html format.
-# text is the usual text-kind of output.
-# LaTeX is handled by MathJaX display mode $$ $$
-# MathML could be tagged differently too.
-FORM_TO_HTML_TAG_FORMAT: Final[Dict[str, str]] = {
-    "System`FullForm": "text",
-    "System`InputForm": "text",
-    "System`MathMLForm": "MathML",
-    "System`OutputForm": "text",
-    "System`TeXForm": "LaTeX",
-    "System`String": "text",
-}
-
 
 def format_output(evaluation, expr, execution_count: int) -> dict:
     """
-    Handle unformatted output using the *specific* capabilities \
-    of mathics-django.
+    Handle unformatted output using the *specific* capabilities
+    of Mathics3 Jupyter
 
     evaluation.py format_output() from which this was derived is \
     similar but it can't make use of a front-ends \
@@ -46,78 +35,82 @@ def format_output(evaluation, expr, execution_count: int) -> dict:
     :param expr: The expression result to format
     """
 
-    def build_content(content: dict, mime_type, value) -> dict:
+    def eval_boxed(result, fn: Callable, obj, **options):
+        try:
+            boxed = fn(evaluation=obj, **options)
+        except BoxError:
+            boxed = None
+            if not hasattr(obj, "seen_box_error"):
+                obj.seen_box_error = True
+                obj.message(
+                    "General",
+                    "notboxes",
+                    Expression(SymbolFullForm, result).evaluate(obj),
+                )
+
+        return boxed
+
+    def build_mime_content(mime_content: dict, mime_type, value) -> dict:
         data = {
             mime_type: value,
         }
-        content["data"] = data
-        return content
+        mime_content["data"] = data
+        return mime_content
+
+    def format_mathml(expr, mime_content: dict) -> dict:
+        boxed = format_element(expr, evaluation, SymbolMathMLForm)
+        if hasattr(boxed, "head") and boxed.head is SymbolInterpretationBox:
+            box_str = boxed.elements[0].value[1:-1]
+            return build_mime_content(mime_content, "text/html", box_str)
+
+        if isinstance(boxed, String) and boxed.value.startswith('"<math'):
+            return build_mime_content(mime_content, "text/html", boxed.value[1:-1])
+
+        boxed = format_element(expr, evaluation, SymbolStandardForm)
+        return build_mime_content(mime_content, "text/plain", str(boxed))
+
+    def format_text(expr, mime_content: dict) -> dict:
+        boxed = format_element(expr, evaluation, SymbolStandardForm)
+        return build_mime_content(mime_content, "text/plain", str(boxed))
 
     # Start with the standard plain text representation
-    content = {"execution_count": execution_count, "metadata": {}}
+    mime_content = {"execution_count": execution_count, "metadata": {}}
 
     if expr is SymbolAborted:
-        return build_content(content, "text/plain", "$Aborted")
+        return build_mime_content(mime_content, "text/html", "<i>$Aborted</i>")
     elif expr is SymbolFailed:
-        return build_content(content, "text/plain", "$Failed")
+        return build_mime_content(mime_content, "text/html", "<i>$Failed</i>")
 
-    # For some expressions, we want formatting to be different.
-    # In particular for FullForm and InputForm output, we don't want
-    # MathML, we want
-    # plain-ol' text so we can cut and paste that.
     expr_type = expr.get_head_name()
-    # For these forms, we strip off the outer "Form" part
-    html_tag_format = FORM_TO_HTML_TAG_FORMAT.get(expr_type, "xml")
+    expr_head = expr.get_head()
 
-    # This part is similar to mathics.core.evaluation.format_output().
-    if html_tag_format == "text":
-        boxed = format_element(expr, evaluation, SymbolOutputForm)
-        if hasattr(boxed, "head") and boxed.head is SymbolInterpretationBox:
-            return build_content(content, "text/plain", boxed.elements[0].value)
+    if expr_head is SymbolMathMLForm:
+        return format_mathml(expr, mime_content)
 
-        result = boxed.boxes_to_text()
-        return build_content(content, "text/plain", result)
+    if expr_head is SymbolString:
+        return build_mime_content(mime_content, "text/plain", f'"{expr.value}"')
 
-    if html_tag_format == "xml":
-        boxed = format_element(expr, evaluation, SymbolStandardForm)
-        if (
-            hasattr(boxed, "head")
-            and boxed.head is SymbolInterpretationBox
-            and (box_value := boxed.elements[0].value).startswith('"<math ')
-        ):
-            # FIXME: [1:-1] is to strip quotes.
-            # We should probably address a long-standing mistake where strings
-            # have quotes in them.
-            return build_content(content, "text/html", box_value)
+    if isinstance(expr, Symbol) and str(expr).startswith("Global`"):
+        return build_mime_content(
+            mime_content, "text/html", f"<i>{expr.short_name}</i>"
+        )
 
-        # This can happen.
-        mathml = boxed.to_mathml(evaluation=evaluation)
-        return build_content(content, "text/html", f"<math>{mathml}</math>")
-
-    if html_tag_format == "LaTeX":
+    if expr_head is SymbolTeXForm:
         boxed = format_element(expr, evaluation, SymbolTeXForm)
-        render_TeXForm_expr = evaluation.parse("Settings`$RenderTeXForm")
         if hasattr(boxed, "head") and boxed.head is SymbolInterpretationBox:
-            # FIXME: [1:-1] is to strip quotes.
-            # We should probably address a long-standing mistake where strings
-            # have quotes in them.
-            box_str_sans_quotes = boxed.elements[0].value[1:-1]
-            render_TeXForm = render_TeXForm_expr.evaluate(evaluation).to_python()
-            if render_TeXForm:
-                box_str_sans_quotes = f"$${box_str_sans_quotes}$$"
-            return build_content(content, "text/latex", box_str_sans_quotes)
+            box_str = boxed.elements[0].value[1:-1]
+            return build_mime_content(mime_content, "text/latex", f"$${box_str}$$")
 
-        # THINK ABOUT: This probably no longer happens
-        if isinstance(boxed, String):
-            return build_content(content, "text/plain", boxed.to_text())
-        else:
-            render_TeXForm = render_TeXForm_expr.evaluate(evaluation).to_python()
-            result = boxed.to_tex(evaluation=evaluation)
-            if render_TeXForm:
-                result = f"$${result}$$"
-                mime_type = "text/latex"
-            else:
-                mime_type = "text/plain"
-            return build_content(content, mime_type, result)
+        box_str = f"$${boxed._value[1:-1]}$$"
+        return build_mime_content(mime_content, "text/latex", box_str)
 
-    return build_content(content, "text/plain", str(expr))
+    elif (form := expr_head) in (
+        SymbolFullForm,
+        SymbolInputForm,
+        SymbolOutputForm,
+        SymbolStandardForm,
+    ):
+        boxed = format_element(expr, evaluation, form)
+        return build_mime_content(mime_content, "text/plain", str(boxed))
+
+    return format_mathml(expr, mime_content)
